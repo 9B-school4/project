95a55b190408a12f5dc3e12aeec997442263aa94
9B-school4 <nedoprogramist009@gmail.com>
2026-05-15 12:28:50 +0300

    Update pawhelp.py

--- school_project/pawhelp.py
+++ school_project/pawhelp.py
@@ -4,10 +4,16 @@ import os
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
 def send_help(message):     
-    bot.send_message(message.chat.id, "Керування відбувається кнопками.")
+    bot.send_message(message.chat.id, "Керування ботом  може відбуватися декількома способами: \n▫ Reply- та Inline-кнопками \n▫Командами з  меню", reply_markup=reply_keyboard())
 
 def send_info(message): 
-    bot.send_message(message.chat.id, "Це інформуючий бот, який зможе допомогти тваринам.")
+    bot.send_message(message.chat.id, "Це інформуючий бот, який у якому ви можете дізнатися як допомогти організаціям з допомоги тваринам.", reply_markup=reply_keyboard())
+
+def send_map(message): 
+    keyboard = InlineKeyboardMarkup()
+    go_to_map = InlineKeyboardButton("Перейти на карту", call_back="map")
+    keyboard.add("Перейти на карту")
+    bot.send_photo(message.chat.id, ,  reply_markup=keyboard)
 
 def reply_keyboard(): 
     rkeyboard = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
@@ -28,28 +34,34 @@ def send_welcome(message):
     bot.send_message(message.chat.id, "Привіт", reply_markup=reply_keyboard())
 
 @bot.message_handler(commands=['info'])
-def send_welcome(message): 
+def send_info(message): 
     send_info()
 
 @bot.message_handler(commands=['help'])
-def send_welcome(message): 
+def send_help(message): 
     send_help()
+    
+@bot.message_handler(commands=['map'])
+def send_map(message):
+    send_map()
 
 @bot.message_handler(func=lambda message: True)
 def handler_message(message):
     if message.text == "Допомога": 
-        bot.send_message(message.chat.id, "Допомога", reply_markup=reply_keyboard())
+        send_help()
     elif message.text == "Інформація": 
-        bot.send_message(message.chat.id, "Це інформуючий бот", reply_markup=reply_keyboard())
+        send_info()
     elif message.text == "Бездомні тварини": 
         bot.send_message(message.chat.id, "Список буде додано пізніше", reply_markup=reply_keyboard())
     elif message.text == "Чим допомогти": 
-        bot.send_message(message.chat.id, "Нічим", reply_markup=reply_keyboard())
+        bot.send_message(message.chat.id, "Поки не було додано жодної тварини.", reply_markup=reply_keyboard())
     elif message.text == "Повідомити про тварину": 
         bot.send_message(message.chat.id, "Тут ви можете повідомити про знайдену безпритульну або пошкоджену тварину", reply_markup=reply_keyboard())
     elif message.text == "Що робити": 
-        bot.send_message(message.chat.id, "Що робити, якщо ви знайшли травмовану тварину", reply_markup=reply_keyboard())
+        bot.send_message(message.chat.id, "Якщо ви знайшшли травмовану, або безпритульну тварину,  ви можете звернутися до:\nВетеринарної клініки: \n*** \nПритулку: \n«Кременчуцькі Хвостики»«КП "Спецсервіс-Кременчук"»: +380 (67) 203 21 97" \
+                        "\n▫«Кременчуцькі Хвостики»: +380 (97) 287 75 44", 
+                         reply_markup=reply_keyboard())
     else: 
-        bot.send_message(message.chat.id, "Керування відбувається кнопками.")
+        send_help()
 
 bot.polling()
